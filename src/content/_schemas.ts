9e26c1ddf4047fc23408a1c4fb684a240c5b0212
s-hirano-ist <solucky0725@icloud.com>
2023-07-01 18:00:39 +0900

    set draft option as required

--- src/content/_schemas.ts
+++ src/content/_schemas.ts
@@ -3,7 +3,7 @@ import { z } from "astro:content";
 export const blogSchema = z
   .object({
     title: z.string(),
-    draft: z.boolean().optional(),
+    draft: z.boolean(),
     description: z.string(),
   })
   .strict();
