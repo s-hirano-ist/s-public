102f2270eb716d9b167a2f650437d9144e16bed3
s-hirano-ist <solucky0725@icloud.com>
2023-07-10 18:36:56 +0900

    draft: introduce sidebar

---
+++ public/toggle-sidebar.js
@@ -0,0 +1,20 @@
+function openNav() {
+  // set the width of the opened navigation bar
+  document.getElementById("post-side-nav").style.width = "600px";
+}
+function closeNav() {
+  // set the width of the closed navigation bar
+  document.getElementById("post-side-nav").style.width = "0px";
+}
+
+window.onload = () => {
+  const openButton = document.querySelector("span.open-sidebar-button");
+  openButton.addEventListener("click", () => {
+    openNav();
+  });
+
+  const closeButton = document.querySelector("span.close-sidebar-button");
+  closeButton.addEventListener("click", () => {
+    closeNav();
+  });
+};
