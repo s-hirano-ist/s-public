b8e153f0e844d12215cd8d83d5dbeb15d4fb958f
s-hirano-ist <solucky0725@icloud.com>
2023-07-11 16:26:08 +0900

    first letter to capital

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,10 +3,9 @@ import type { BlogFrontmatter } from "@content/_schemas";
 export interface Props {
   href?: string;
   frontmatter: BlogFrontmatter;
-  secHeading?: boolean;
 }
 
-export default function Card({ href, frontmatter, secHeading = true }: Props) {
+export default function Card({ href, frontmatter }: Props) {
   const { title, description } = frontmatter;
   return (
     <li className="my-6">
@@ -14,15 +13,9 @@ export default function Card({ href, frontmatter, secHeading = true }: Props) {
         href={href}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
       >
-        {secHeading ? (
-          <h2 className="text-lg font-medium decoration-dashed hover:underline">
-            {title}
-          </h2>
-        ) : (
-          <h3 className="text-lg font-medium decoration-dashed hover:underline">
-            {title}
-          </h3>
-        )}
+        <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          {title.charAt(0).toUpperCase() + title.slice(1).toLowerCase()}
+        </h2>
       </a>
       <p>{description}</p>
     </li>
