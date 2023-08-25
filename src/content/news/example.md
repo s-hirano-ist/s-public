8fe3923e1c81ee59ab1026ec2b4ff478af62db62
s-hirano-ist <solucky0725@icloud.com>
2023-08-25 18:19:32 +0900

    feat: add news contents

---
+++ src/content/news/example.md
@@ -0,0 +1,9 @@
+---
+title: サンプル記事
+description: 準備中
+draft: false
+---
+
+## サンプル記事
+
+準備中。
--- src/utils/getSortedPosts.ts
+++ src/utils/getSortedPosts.ts
@@ -1,6 +1,6 @@
 import type { CollectionEntry } from "astro:content";
 
-const getSortedPosts = (posts: CollectionEntry<"summary">[]) =>
+const getSortedPosts = (posts: CollectionEntry<"news" | "summary">[]) =>
   posts.filter(({ data }) => !data.draft);
 
 export default getSortedPosts;
