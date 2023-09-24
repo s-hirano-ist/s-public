aa34d38113d6d212e9e620913eb2b5d5cdb76767
s-hirano-ist <solucky0725@icloud.com>
2023-09-24 12:04:57 +0900

    code migration

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -2,6 +2,6 @@ import type { JsonFrontmatter, MarkdownFrontmatter } from "@content/_schemas";
 import { slug as slugger } from "github-slugger";
 
 const slugify = (post: MarkdownFrontmatter | JsonFrontmatter) =>
-  slugger(post.title);
+  slugger(post.heading);
 
 export default slugify;
