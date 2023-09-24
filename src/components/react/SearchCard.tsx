3f05f38b2da397428a1b7b760aec04d176a8b1e0
Soraki Hirano <88515957+s-hirano-ist@users.noreply.github.com>
2023-09-24 20:49:44 +0900

    fix: search bar (#203)
    * add tsc compile options
    
    * fix commands
    
    * fix: search
    
    * search for both news and summary
    
    * docs: update readme
    
    * fix: package json

---
+++ src/components/react/SearchCard.tsx
@@ -0,0 +1,23 @@
+import { capitalizeFirstLetterOnly } from "@utils/convertString";
+
+interface Props {
+  href: string;
+  title: string;
+  description: string;
+}
+
+export default function SearchCard({ href, title, description }: Props) {
+  return (
+    <li className="my-6">
+      <a
+        href={href}
+        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+      >
+        <h2 className="text-lg font-medium decoration-dashed hover:underline">
+          {capitalizeFirstLetterOnly(title)}
+        </h2>
+      </a>
+      <p>{description}</p>
+    </li>
+  );
+}
