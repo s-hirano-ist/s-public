8fd39b27368e128b34fa60c913e682d1338e5763
Soraki Hirano <88515957+s-hirano-ist@users.noreply.github.com>
2023-10-04 09:17:51 +0900

    refactor: prettier settings (#225)

--- src/components/react/SearchCard.tsx
+++ src/components/react/SearchCard.tsx
@@ -11,8 +11,7 @@ export default function SearchCard({ href, title, description }: Props) {
     <li className="my-6">
       <a
         href={href}
-        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
-      >
+        className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0">
         <h2 className="text-lg font-medium decoration-dashed hover:underline">
           {capitalizeFirstLetterOnly(title)}
         </h2>
