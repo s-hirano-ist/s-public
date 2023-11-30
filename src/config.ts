ad54e27e80e9ad02878f33aec090285beb815b68
s-hirano-ist <solucky0725@icloud.com>
2023-11-30 09:49:55 +0900

    feat: update contents title and description for release to public (#390)
    * fix: title, author, and description of blog
    
    * fix: contents descriptions
    
    * fix: dplan layout
    
    * add links

--- src/config.ts
+++ src/config.ts
@@ -5,13 +5,15 @@ type Site = {
   title: string;
   ogImage: string;
   postPerPage: number;
+  email: string;
 };
 
 export const SITE: Site = {
   website: "https://s-hirano.com/",
   author: "s-hirano-ist",
-  description: "Thoughts by s-hirano-ist.",
+  description: "Portfolio and blog of s-hirano-ist.",
   title: "思考の備忘録",
   ogImage: "/defaultOgImage.jpg",
   postPerPage: 10,
+  email: "s-hirano-ist@outlook.com",
 };
