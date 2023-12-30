fc1be7b4699ccd2a20311d3cbf01620b00795d52
Soraki Hirano <solucky0725@icloud.com>
2023-12-30 15:43:43 +0900

    feat: add rehype remark settings (#501)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import remarkToc from "remark-toc";
+// eslint-disable-next-line no-restricted-imports
 import { SITE } from "./src/config";
 
 const IS_DEV = import.meta.env.MODE === "development";
@@ -26,6 +30,22 @@ export default defineConfig({
       theme: "one-dark-pro",
       wrap: true,
     },
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+          properties: {},
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: { className: ["icon-header-link", "fa", "fa-link"] },
+          },
+        },
+      ],
+    ],
+    remarkPlugins: [[remarkToc, { heading: "目次" }]],
     extendDefaultPlugins: true,
   },
   // not necessary for static sites. Only for SSR.
