7f71462967ae30a00b6044ce122530753660b969
s-hirano-ist <solucky0725@icloud.com>
2024-01-18 17:28:17 +0900

    refactor: remove TODO comments

--- src/components/react/SearchCard.tsx
+++ src/components/react/SearchCard.tsx
@@ -12,7 +12,6 @@ export default function SearchCard({ href, title, description }: Props) {
       <a
         href={href}
         className="inline-block decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0">
-        {/* TODO: what those css mean */}
         <h2 className="text-lg font-medium text-primary decoration-dashed hover:underline">
           {capitalizeFirstLetterOnly(title)}
         </h2>
