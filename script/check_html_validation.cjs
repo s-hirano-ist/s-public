99c97c83772975ef470b278e757f97b28d13e68b
s-hirano-ist <solucky0725@icloud.com>
2024-01-20 12:06:27 +0900

    delete vnu checker raw image

--- script/check_html_validation.cjs
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-// eslint-disable-next-line
-const { execFile } = require("child_process");
-// eslint-disable-next-line
-const vnu = require("vnu-jar");
-
-// Print path to vnu.jar
-console.log(vnu);
