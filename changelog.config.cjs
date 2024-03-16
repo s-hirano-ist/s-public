51ecb255dbcf1e89b8630abf29036a5b9ca1c7f4
Soraki Hirano <solucky0725@icloud.com>
2024-03-16 21:30:45 +0900

    chore: add git cz (#599)
    * chore: add library
    
    * chore: add config file for git-cz
    
    * contents: coding spirit
    
    * chore: new line with lf rather thant crlf on windows
    
    Closes: #581

---
+++ changelog.config.cjs
@@ -0,0 +1,63 @@
+module.exports = {
+  disableEmoji: true,
+  list: [
+    "chore",
+    "ci",
+    "docs",
+    "feat",
+    "fix",
+    "refactor",
+    "style",
+    "test",
+    "contents",
+  ],
+  maxMessageLength: 64,
+  minMessageLength: 3,
+  questions: [
+    "type",
+    "scope",
+    "subject",
+    "body",
+    "breaking",
+    "issues",
+    "lerna",
+  ],
+  types: {
+    chore: {
+      description: "Chore changes",
+      value: "chore",
+    },
+    ci: {
+      description: "Changes in CI/CD pipeline",
+      value: "ci",
+    },
+    docs: {
+      description: "Update README.md or other documentation files",
+      value: "docs",
+    },
+    feat: {
+      description: "New features",
+      value: "feat",
+    },
+    fix: {
+      description: "Bug fixes",
+      value: "fix",
+    },
+    refactor: {
+      description: "Refactoring",
+      value: "refactor",
+    },
+    style: {
+      description: "Change styles",
+      value: "style",
+    },
+    test: {
+      description: "Add or update tests",
+      value: "test",
+    },
+    contents: {
+      description: "Add blog contents",
+      value: "contents",
+    },
+  },
+};
