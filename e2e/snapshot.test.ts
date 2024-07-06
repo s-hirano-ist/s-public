c915c05f43bebd00d223a3137cde8b4b764e58e2
s-hirano-ist <s-hirano-ist.outlook.com>
2024-07-06 09:15:44 +0900

    chore: disable playwright test
    Closes: #664

--- e2e/snapshot.test.ts
+++ e2e/snapshot.test.ts
@@ -1,71 +1,71 @@
-import { test, expect } from "@playwright/test";
+// import { test, expect } from "@playwright/test";
 
-const pagePath = [
-  {
-    name: "root",
-    path: "/",
-    fullPage: true,
-  },
-  {
-    name: "news",
-    path: "/news",
-    fullPage: true,
-  },
-  {
-    name: "news/[slug]",
-    path: "/news/artificial-intelligence",
-    fullPage: false,
-  },
-  {
-    name: "summary",
-    path: "/summary",
-    fullPage: true,
-  },
-  {
-    name: "summary/[slug]",
-    path: "/summary/architecture",
-    fullPage: true,
-  },
-  {
-    name: "book",
-    path: "/book",
-    fullPage: false,
-  },
-  {
-    name: "blog",
-    path: "/blog",
-    fullPage: true,
-  },
-  {
-    name: "blog/[slug]",
-    path: "/blog/d-plan",
-    fullPage: true,
-  },
-  {
-    name: "tech-stack",
-    path: "/tech-stack",
-    fullPage: false,
-  },
-  {
-    name: "photo",
-    path: "/photo",
-    fullPage: false,
-  },
-  {
-    name: "diy",
-    path: "/diy",
-    fullPage: false,
-  },
-];
+// const pagePath = [
+//   {
+//     name: "root",
+//     path: "/",
+//     fullPage: true,
+//   },
+//   {
+//     name: "news",
+//     path: "/news",
+//     fullPage: true,
+//   },
+//   {
+//     name: "news/[slug]",
+//     path: "/news/artificial-intelligence",
+//     fullPage: false,
+//   },
+//   {
+//     name: "summary",
+//     path: "/summary",
+//     fullPage: true,
+//   },
+//   {
+//     name: "summary/[slug]",
+//     path: "/summary/architecture",
+//     fullPage: true,
+//   },
+//   {
+//     name: "book",
+//     path: "/book",
+//     fullPage: false,
+//   },
+//   {
+//     name: "blog",
+//     path: "/blog",
+//     fullPage: true,
+//   },
+//   {
+//     name: "blog/[slug]",
+//     path: "/blog/d-plan",
+//     fullPage: true,
+//   },
+//   {
+//     name: "tech-stack",
+//     path: "/tech-stack",
+//     fullPage: false,
+//   },
+//   {
+//     name: "photo",
+//     path: "/photo",
+//     fullPage: false,
+//   },
+//   {
+//     name: "diy",
+//     path: "/diy",
+//     fullPage: false,
+//   },
+// ];
 
-test.describe.parallel("Visual regression testing of page", () => {
-  pagePath.map(item => {
-    test(`snapshot test ${item.name}`, async ({ page }) => {
-      await page.goto(`${item.path}`);
-      await expect(page).toHaveScreenshot({
-        fullPage: item.fullPage,
-        animations: "disabled",
-      });
-    });
-  });
-});
+// test.describe.parallel("Visual regression testing of page", () => {
+//   pagePath.map(item => {
+//     test(`snapshot test ${item.name}`, async ({ page }) => {
+//       await page.goto(`${item.path}`);
+//       await expect(page).toHaveScreenshot({
+//         fullPage: item.fullPage,
+//         animations: "disabled",
+//       });
+//     });
+//   });
+// });
--- playwright.config.ts
+++ playwright.config.ts
@@ -1,57 +1,57 @@
-import type { PlaywrightTestConfig } from "@playwright/test";
-import { devices } from "@playwright/test";
+// import type { PlaywrightTestConfig } from "@playwright/test";
+// import { devices } from "@playwright/test";
 
-const config: PlaywrightTestConfig = {
-  testDir: "./e2e",
-  fullyParallel: true,
-  reporter: "html",
-  expect: { timeout: 10000 },
-  use: {
-    baseURL: process.env.BASE_URL,
-    trace: "on-first-retry",
-  },
-  forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
-    // FIXME: page crashes on other devices than chromium
-    // {
-    //   name: "firefox",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-    // {
-    //   name: "webkit",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
-    // {
-    //   name: "iPhone 14 Pro",
-    //   use: { ...devices["iPhone 14 Pro"] },
-    // },
-    // {
-    //   name: "iPhone SE",
-    //   use: { ...devices["iPhone SE"] },
-    // },
-    // {
-    //   name: "iPad Mini",
-    //   use: { ...devices["iPad Mini"] },
-    // },
-    // {
-    //   name: "iPad Mini landscape",
-    //   use: { ...devices["iPad Mini landscape"] },
-    // },
-    // {
-    //   name: "Pixel 5",
-    //   use: { ...devices["Pixel 5"] },
-    // },
-  ],
-  // webServer: {
-  //   command: "pnpm preview",
-  //   port: 4321,
-  //   reuseExistingServer: true,
-  // },
-};
+// const config: PlaywrightTestConfig = {
+//   testDir: "./e2e",
+//   fullyParallel: true,
+//   reporter: "html",
+//   expect: { timeout: 10000 },
+//   use: {
+//     baseURL: process.env.BASE_URL,
+//     trace: "on-first-retry",
+//   },
+//   forbidOnly: !!process.env.CI,
+//   retries: process.env.CI ? 2 : 0,
+//   projects: [
+//     {
+//       name: "chromium",
+//       use: { ...devices["Desktop Chrome"] },
+//     },
+//     // FIXME: page crashes on other devices than chromium
+//     // {
+//     //   name: "firefox",
+//     //   use: { ...devices["Desktop Firefox"] },
+//     // },
+//     // {
+//     //   name: "webkit",
+//     //   use: { ...devices["Desktop Safari"] },
+//     // },
+//     // {
+//     //   name: "iPhone 14 Pro",
+//     //   use: { ...devices["iPhone 14 Pro"] },
+//     // },
+//     // {
+//     //   name: "iPhone SE",
+//     //   use: { ...devices["iPhone SE"] },
+//     // },
+//     // {
+//     //   name: "iPad Mini",
+//     //   use: { ...devices["iPad Mini"] },
+//     // },
+//     // {
+//     //   name: "iPad Mini landscape",
+//     //   use: { ...devices["iPad Mini landscape"] },
+//     // },
+//     // {
+//     //   name: "Pixel 5",
+//     //   use: { ...devices["Pixel 5"] },
+//     // },
+//   ],
+//   // webServer: {
+//   //   command: "pnpm preview",
+//   //   port: 4321,
+//   //   reuseExistingServer: true,
+//   // },
+// };
 
-export default config;
+// export default config;
