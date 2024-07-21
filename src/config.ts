27bfc032cfba5eac7f243bcd7e26d78581b00e64
Soraki Hirano <solucky0725@icloud.com>
2024-07-21 19:38:28 +0900

    chore: change repository name (#712)
    Co-authored-by: s-hirano-ist <s-hirano-ist.outlook.com>

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ type Site = {
 export const SITE: Site = {
   website: "https://s-hirano.com/",
   author: "s-hirano-ist",
-  description: "Portfolio and blog of s-hirano-ist.",
+  description: "Portfolio and blog pages of s-hirano-ist.",
   title: "思考の備忘録",
   ogImage: "/portfolio.webp",
   email: "s-hirano-ist@outlook.com",
