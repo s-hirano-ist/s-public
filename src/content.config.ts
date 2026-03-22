fa59ce8fe32b1abda6d7976ae8295995a96ca78b
Sola <s-hirano-ist@outlook.com>
2026-03-22 20:11:41 +0900

    feat: add more blogs (#1257)

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,6 +5,7 @@ const blog = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     heading: z.string(),
+    slug: z.string(),
     description: z.string(),
     draft: z.boolean(),
     date: z.coerce.date(),
--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,6 +1,5 @@
 import type { CollectionEntry } from "astro:content";
-import slug from "slug";
 
-const slugify = (post: CollectionEntry<"blog">["data"]) => slug(post.heading);
+const slugify = (post: CollectionEntry<"blog">["data"]) => post.slug;
 
 export default slugify;
